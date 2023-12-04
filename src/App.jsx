9e1ef2e57c8c16e59842b8feb6dee1deb993f2e7
shivamKcode <shivambaniya246@gmail.com>
2023-12-03 23:15:57 -0500

    Add existing project files to Git

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import {createRoot} from 'react-dom/client' 
+import './App.css'
+import Quote from './components/quote'
+
+createRoot(document.getElementById('root')).render(<Quote/>)
---
+++ src/components/quote.jsx
@@ -0,0 +1,56 @@
+import React from 'react'
+import axios from 'axios'
+
+const Quote = () => {
+    const [quote, setQuote] = React.useState('')
+    const [displayedQuote, setDisplayedQuote] = React.useState('');
+//   const [currentCharIndex, setCurrentCharIndex] = React.useState(0);
+
+    const fetchQuote = async () => {
+        const response = await axios.get('https://api.adviceslip.com/advice');
+        setQuote(response.data.slip.advice);
+        displayQuoteWithDelay(response.data.slip.advice);
+    }
+    
+    const speakQuote = (quote) =>{
+        if ('speechSynthesis' in window) {
+            const speech = new SpeechSynthesisUtterance(quote);
+            speech.lang = 'en-US';
+            speech.volume = 1;
+            speech.rate = 1;
+            speech.pitch = 1;
+            window.speechSynthesis.speak(speech);
+        } else {
+            console.error('Speech synthesis not supported');
+        }
+    }
+    // React.useEffect(() => {
+        
+    // }, [])
+    
+    const displayQuoteWithDelay = async () => {
+        const words = quote.split(' ');
+        for (let i = 0; i < words.length; i++) {
+          setDisplayedQuote(words.slice(0, i + 1).join(' '));
+          await new Promise((resolve) => setTimeout(resolve, 200));
+        }
+        speakQuote(quote);
+      };
+    
+      const handleNewQuote = () => {
+        setQuote('');
+        setDisplayedQuote('');
+        fetchQuote();
+      };
+    
+    return (
+        <div className='app'>
+            <div className='random-quote'>
+                <h2 className='quote-text' >{displayedQuote}</h2>
+                <button onClick={handleNewQuote}>New Quote</button>
+            </div>
+        </div>
+    )
+}
+
+export default Quote
