c8bc98779de1329491e303badc7fa03e1321bc30
shivamKcode <shivambaniya246@gmail.com>
2023-12-04 17:07:14 -0500

    changes to useEffect

--- src/components/quote.jsx
+++ src/components/quote.jsx
@@ -12,18 +12,50 @@ const Quote = () => {
 
         const display = async () => await displayQuoteWithDelay(advice);
         display()
+
+        speakQuote(quote);
+
     }
 
+    // const speakQuote = (quote) => {
+    //     if ('speechSynthesis' in window) {
+    //         const speech = new SpeechSynthesisUtterance(quote);
+    //         speech.lang = 'en-US';
+    //         speech.volume = 1;
+    //         speech.rate = 1;
+    //         speech.pitch = 1;
+    //         window.speechSynthesis.speak(speech);
+    //     } else {
+    //         console.error('Speech synthesis not supported');
+    //     }
+    // }
+
     const speakQuote = (quote) => {
-        if ('speechSynthesis' in window) {
-            const speech = new SpeechSynthesisUtterance(quote);
-            speech.lang = 'en-US';
-            speech.volume = 1;
-            speech.rate = 1;
-            speech.pitch = 1;
-            window.speechSynthesis.speak(speech);
+        const speak = () => {
+            if ('speechSynthesis' in window) {
+                // Stop any ongoing speech
+                window.speechSynthesis.cancel();
+    
+                const speech = new SpeechSynthesisUtterance(quote);
+                speech.lang = 'en-US';
+                speech.volume = 1;
+                speech.rate = 1;
+                speech.pitch = 1.5;
+                window.speechSynthesis.speak(speech);
+            } else {
+                console.error('Speech synthesis not supported');
+            }
+        }
+    
+        if (window.speechSynthesis !== undefined) {
+            speak();
         } else {
-            console.error('Speech synthesis not supported');
+            const id = setInterval(() => {
+                if (window.speechSynthesis !== undefined) {
+                    speak();
+                    clearInterval(id);
+                }
+            }, 10);
         }
     }
 
@@ -33,8 +65,8 @@ const Quote = () => {
             setDisplayedQuote(words.slice(0, i + 1).join(' '));
             await new Promise((resolve) => setTimeout(resolve, 200));
         }
-        console.log(quote)
-        speakQuote(quote);
+        // console.log(quote)
+        // speakQuote(quote);
     };
 
     const handleNewQuote = () => {
@@ -47,6 +79,12 @@ const Quote = () => {
         handleNewQuote()
     // eslint-disable-next-line react-hooks/exhaustive-deps
     },[])
+
+    React.useEffect(() => {
+        if (quote !== '') {
+            speakQuote(quote);
+        }
+    }, [quote]);
 
     return (
         <div className='app'>
